5bd1481910f3562b240a450b8ed08f3bf30c897f
Kai-Stevens <kai.stevens97@gmail.com>
2022-11-09 10:52:19 +0000

    setup express server, works with a base route

---
+++ api.js
@@ -0,0 +1,26 @@
+// external imports
+const express = require('express');
+const cors = require('cors');
+
+// internal imports
+const logRoutes = require('./middleware/logger');
+// routes
+// const gameRouter = require('./routers/game');
+
+// configuration
+const api = express();
+api.use(cors());
+api.use(express.json());
+api.use(logRoutes);
+
+// base route
+api.get("/", (req, res) => {
+    res.json({
+        name: "Humanties App API",
+        description: "An API to support our Humanities Game App"
+    });
+});
+
+// api.use("/games", gameRouter);
+
+module.exports = api;
