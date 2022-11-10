2b8c054317da2cb7d078f735275088a70af41e61
Kai-Stevens <kai.stevens97@gmail.com>
2022-11-10 15:55:45 +0000

    reduce needless info in the database queries for adventure game model

--- models/AdventureGame.js
+++ models/AdventureGame.js
@@ -14,20 +14,20 @@ class AdventureGame extends Game {
     for (let i = 0; i < adventureGames.length; i++) {
       //adventureGames[i] = current adventure game
       // for each one, i want to get that game's scenes and append it
-      const sceneResponse = await db.query("SELECT * FROM scene WHERE game_id = $1", [
+      const sceneResponse = await db.query("SELECT scene_id, bg_img FROM scene WHERE game_id = $1", [
         adventureGames[i]["game_id"],
       ]);
       const scenes = sceneResponse.rows;
 
       // for each games scene, get that scenes items and chats, and append it to the object
       for (let j = 0; j < scenes.length; j++) {
-        const sceneItemsResponse = await db.query("SELECT * FROM item WHERE scene_id = $1", [
+        const sceneItemsResponse = await db.query("SELECT item_name, item_text_event, item_required FROM item WHERE scene_id = $1", [
           scenes[j]["scene_id"],
         ]);
         const sceneItems = sceneItemsResponse.rows;
         scenes[j]["items"] = sceneItems;
 
-        const sceneChatsResponse = await db.query("SELECT * FROM chat WHERE scene_id = $1", [
+        const sceneChatsResponse = await db.query("SELECT chat_text FROM chat WHERE scene_id = $1", [
           scenes[j]["scene_id"],
         ]);
         const sceneChats = sceneChatsResponse.rows;
@@ -51,18 +51,18 @@ class AdventureGame extends Game {
     }
 
     // get that game's scenes
-    const sceneResponse = await db.query("SELECT * FROM scene WHERE game_id = $1", [id]);
+    const sceneResponse = await db.query("SELECT scene_id, bg_img FROM scene WHERE game_id = $1", [id]);
     const scenes = sceneResponse.rows;
 
     // for scene, get that scene's items and chats, and append it to the object
     for (let i = 0; i < scenes.length; i++) {
-      const sceneItemsResponse = await db.query("SELECT * FROM item WHERE scene_id = $1", [
+      const sceneItemsResponse = await db.query("SELECT item_name, item_text_event, item_required FROM item WHERE scene_id = $1", [
         scenes[i]["scene_id"],
       ]);
       const sceneItems = sceneItemsResponse.rows;
       scenes[i]["items"] = sceneItems;
 
-      const sceneChatsResponse = await db.query("SELECT * FROM chat WHERE scene_id = $1", [
+      const sceneChatsResponse = await db.query("SELECT chat_text FROM chat WHERE scene_id = $1", [
         scenes[i]["scene_id"],
       ]);
       const sceneChats = sceneChatsResponse.rows;
